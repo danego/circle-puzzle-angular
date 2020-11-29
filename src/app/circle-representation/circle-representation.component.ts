6c51bee60f016edd0cf2fc0cbd6754beb568062b
Dane <nevesdane@gmail.com>
2020-11-29 03:32:32 -0800

    started process of drag and drop. Layer one working halfway

--- src/app/circle-representation/circle-representation.component.ts
+++ src/app/circle-representation/circle-representation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-circle-representation',
@@ -13,18 +15,25 @@ export class CircleRepresentationComponent implements OnInit {
   ngOnInit() {
     this.pieces = new Array(4);
     this.pieces[0] = ['G', 'G', 'G', 'P', 'P', 'P', 'G', 'G', 'P', 'P'];
-    this.pieces[1] = [
-      {top:'G', left:'P', right:'O'},
-      {top:'G', left:'G', right:'P'},
-      {top:'G', left:'O', right:'G'},
-      {top:'G', left:'P', right:'P'},
-      {top:'G', left:'O', right:'O'},
-      {top:'P', left:'P', right:'O'},
-      {top:'P', left:'O', right:'P'},
-      {top:'P', left:'G', right:'P'},
-      {top:'P', left:'O', right:'G'},
-      {top:'P', left:'G', right:'G'}
-    ];
+
+    //each piece should be in its own array for drag-&-drop
+    this.pieces[1] = new Array(10);
+    for (let i = 0; i < 10; i++) {
+      this.pieces[1][i] = [];
+    }
+    //array[layer][d&d/piece][actual piece]
+    this.pieces[1][0][0] = {top:'G', left:'P', right:'O'};
+    this.pieces[1][1][0] = {top:'G', left:'G', right:'P'};
+    this.pieces[1][2][0] = {top:'G', left:'O', right:'G'};
+    this.pieces[1][3][0] = {top:'G', left:'P', right:'P'};
+    this.pieces[1][4][0] = {top:'G', left:'O', right:'O'};
+    this.pieces[1][5][0] = {top:'P', left:'P', right:'O'};
+    this.pieces[1][6][0] = {top:'P', left:'O', right:'P'};
+    this.pieces[1][7][0] = {top:'P', left:'G', right:'P'};
+    this.pieces[1][8][0] = {top:'P', left:'O', right:'G'};
+    this.pieces[1][9][0] = {top:'P', left:'G', right:'G'};  
+
+
     this.pieces[2] = [
       {left:'G', right:'P', bottom:'O'},
       {left:'O', right:'G', bottom:'P'},
@@ -46,4 +55,13 @@ export class CircleRepresentationComponent implements OnInit {
     ];
   }
 
+  dropped(event: CdkDragDrop<string[]>) {
+    //move to new, dropped list
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
